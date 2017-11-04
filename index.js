57b918791c61edbd54dcc2de5699262526d06a6a
John Otander <johnotander@gmail.com>
2017-11-04 14:47:48 -0600

    Hello, world

---
+++ index.js
@@ -0,0 +1,100 @@
+const classPostfix = require('postcss-class-postfix')
+const classPrefix = require('postcss-class-prefix')
+const stripComments = require('strip-css-comments')
+const isNumber = require('is-number')
+const isObject = require('isobject')
+const postcss = require('postcss')
+
+const DEFAULTS = ['md', 'lg', 'xl', 'xxl']
+
+module.exports = async (css, mqs) => {
+  const px = parse(mqs)
+		.map(({ key, mq, prefix, delimiter = '-' }) => {
+			const fn = prefix ?
+				classPrefix(`${key}${delimiter}`) :
+				classPostfix(`${delimiter}${key}`)
+
+			return postcss([ fn ])
+				.process(css)
+				.then(mqified => `
+					@media ${mq} {
+						${stripComments(mqified.css).trim()}
+					}
+				`)
+		})
+
+  const mqCss = await Promise.all(px)
+
+  return [css].concat(mqCss).join('\n')
+}
+
+const format = mediaQueries => mediaQueries.map((breakpoint, i) => {
+	if (isNumber(breakpoint)) {
+		return {
+			key: DEFAULTS[i] || `mq${i}`,
+			value: breakpoint
+		}
+	}
+
+  const key = Object.keys(breakpoint)[0]
+  const value = breakpoint[key]
+
+  if (isObject(value)) {
+    return Object.assign({}, breakpoint[key], {
+      key,
+      value: value.value
+    })
+  } else {
+    return { key, value }
+  }
+})
+
+const parse = mediaQueries => {
+  const formatted = format(mediaQueries)
+
+  const minMaxMedia = formatted.filter(s => !isMinOrMaxWidthQuery(s))
+  const minMaxQueries = minMaxMedia.map((breakpoint, i) => {
+    const nextBreakpoint = minMaxMedia[i+1]
+
+    const mq = [
+      'screen',
+      `(min-width: ${withUnits(breakpoint.value)})`
+    ]
+
+    if (nextBreakpoint) {
+      mq.push(`(max-width: ${withUnits(nextBreakpoint.value)})`)
+    }
+
+    return Object.assign({}, breakpoint, { mq: mq.join(' and ') })
+  })
+
+  const minQueries = formatted
+    .filter(isMinWidthQuery)
+    .map(breakpoint => {
+      const mq = `screen and (min-width: ${withUnits(breakpoint.value)})`
+
+      return Object.assign({}, breakpoint, { mq })
+    })
+
+  const maxQueries = formatted
+    .filter(isMaxWidthQuery)
+    .map(breakpoint => {
+      const mq = `screen and (max-width: ${withUnits(breakpoint.value)})`
+
+      return Object.assign({}, breakpoint, { mq })
+    })
+
+  return minMaxQueries.concat(minQueries).concat(maxQueries)
+}
+
+const isMinWidthQuery = breakpoint => breakpoint.minWidth
+const isMaxWidthQuery = breakpoint => breakpoint.maxWidth
+const isMinOrMaxWidthQuery = breakpoint => breakpoint.minWidth || breakpoint.maxWidth
+
+const hasUnits = str => /(px|em|rem)$/.test(str)
+const withUnits = (str, unit = 'em') => hasUnits(str) ? str : `${str}${unit}`
+
+module.exports.format = format
+module.exports.parse = parse
+module.exports.hasUnits = hasUnits
+module.exports.withUnits = withUnits
---
+++ test.js
@@ -0,0 +1,44 @@
+import test from 'ava'
+import mqify from './'
+
+const INPUT = `
+.fl { float: left }
+.fr { float: right }
+`
+
+test('mqifies an array', async t => {
+  t.snapshot(await mqify(INPUT, [24, 32, 64]))
+})
+
+const COMPLEX = [
+  { m: '32' },
+  {
+    l: {
+      value: '64'
+    }
+  },
+  {
+    med: {
+      value: 64,
+      minWidth: true,
+      prefix: true,
+      delimiter: '@'
+    }
+  },
+  {
+    xl: {
+      value: 123,
+      minWidth: true
+    }
+  },
+  {
+    ns: {
+      value: 1234,
+      maxWidth: true
+    }
+  }
+]
+
+test('handles all the options', async t => {
+	t.snapshot(await mqify(INPUT, COMPLEX))
+})
